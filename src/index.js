d3490e343986590bfe62e26adf4e65bedb5d2171
csluduena <csluduena@gmail.com>
2025-03-31 14:24:04 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles.scss';
+import 'bootstrap/js/dist/collapse';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// Inicializar AOS para animaciones al hacer scroll
+AOS.init({
+  duration: 800,
+  once: true
+});
